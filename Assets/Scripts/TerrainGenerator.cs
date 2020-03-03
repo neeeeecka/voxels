4871a79c3b2f7af6e485ce46af1b586819102c6b
blackstormx <blackshtormx@gmail.com>
2020-03-03 20:24:42 +0400

    updates

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -3,110 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Face
+public static class CubeMeshData
 {
-    List<Vector3> vertices;
-    List<int> triangles;
-    List<Vector3> normals;
-    List<Vector2> uv;
-    public Face(
-        Vector3 direction,
-        int startAt,
-        Vector3[] verArr,
-        int[] triArr,
-        Vector3[] normArr,
-        Vector2[] uvArr
-        )
+    public static Vector3[] vertices = {
+        new Vector3(1,1,1),
+        new Vector3(-1,1,1),
+        new Vector3(-1,-1,1),
+        new Vector3(1,-1,1),
+        new Vector3(-1,1,-1),
+        new Vector3(1,1,-1),
+        new Vector3(1,-1,-1),
+        new Vector3(-1,-1,-1),
+    };
+
+    public static int[][] faceTriangles = {
+        new int[]{0,1,2,3},
+        new int[]{5,0,3,6},
+        new int[]{4,5,6,7},
+        new int[]{1,4,7,2},
+        new int[]{5,4,1,0},
+        new int[]{3,2,7,6}
+    };
+
+    public static Vector3[] faceVertices(int dir)
     {
-        vertices = new List<Vector3>(verArr);
-        triangles = new List<int>(triArr);
-        normals = new List<Vector3>(normArr);
-        uv = new List<Vector2>(uvArr);
-
-        // Vector3 startVert = vertices[startVertIndex];
-
-        int lastVertIndex = vertices.Count - 1;
-        if (lastVertIndex <= 0)
+        Vector3[] fv = new Vector3[4];
+        for (int i = 0; i < 4; i++)
         {
-            lastVertIndex = 0;
+            fv[i] = vertices[faceTriangles[dir][i]];
         }
-
-        //last + 0
-        vertices.Add(new Vector3(0, 0, 0));
-
-        //last +1
-        vertices.Add(new Vector3(1, 0, 0));
-
-        //last +2
-        vertices.Add(new Vector3(0, 1, 0));
-
-        //last +3
-        vertices.Add(new Vector3(1, 1, 0));
-
-        //first triangle
-        triangles.Add(lastVertIndex);
-        triangles.Add(lastVertIndex + 2);
-        triangles.Add(lastVertIndex + 1);
-
-        //second triangle
-        triangles.Add(lastVertIndex + 2);
-        triangles.Add(lastVertIndex + 3);
-        triangles.Add(lastVertIndex + 1);
-
-
-        //add normals
-        for (int i = 0; i < vertices.Count; i++)
-        {
-            normals.Add(-Vector3.forward);
-        }
-        //add UVs
-        uv.Add(new Vector2(0, 0));
-        uv.Add(new Vector2(1, 0));
-        uv.Add(new Vector2(0, 1));
-        uv.Add(new Vector2(1, 1));
+        return fv;
     }
-
-    public Vector3[] GetVertices()
-    {
-        return vertices.ToArray();
-    }
-    public int[] GetTriangles()
-    {
-        return triangles.ToArray();
-    }
-
-    public Vector3[] GetNormals()
-    {
-        return normals.ToArray();
-    }
-    public Vector2[] GetUVs()
-    {
-        return uv.ToArray();
-    }
-
 }
 
 public class TerrainGenerator : MonoBehaviour
 {
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    Mesh mesh;
 
     void Start()
     {
-        Mesh mesh = new Mesh();
+        mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+        MakeCube();
+        UpdateMesh();
+    }
 
-        Face face = new Face(
-            new Vector3(0, 0, 0),
-            0,
-            new Vector3[] { },
-            new int[] { },
-            new Vector3[] { },
-            new Vector2[] { }
-             );
+    void MakeCube()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            MakeFace(i);
+        }
+    }
+    void MakeFace(int dir)
+    {
+        vertices.AddRange(CubeMeshData.faceVertices(dir));
+        int zero = vertices.Count - 4;
 
-        mesh.vertices = face.GetVertices();
-        mesh.triangles = face.GetTriangles();
-        mesh.normals = face.GetNormals();
-        mesh.uv = face.GetUVs();
+        triangles.Add(zero);
+        triangles.Add(zero + 1);
+        triangles.Add(zero + 2);
+        triangles.Add(zero);
+        triangles.Add(zero + 2);
+        triangles.Add(zero + 3);
+    }
+
+    void UpdateMesh()
+    {
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
 
     }
 }
