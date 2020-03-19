9416a3583b5efc52fdbfd4c1c2ebeaee8baff06b
blackstormx <blackshtormx@gmail.com>
2020-03-19 22:29:36 +0400

    many chunks

--- Assets/Game Scripts/ChunkVoxelData.cs
+++ Assets/Game Scripts/ChunkVoxelData.cs
@@ -1,25 +1,221 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class ChunkVoxelData : MonoBehaviour
 {
 
-    public int[] raw = new int[1 * 1 * 1];
-    public int size = 0;
+    private int[] raw = new int[32*32*32];
+    public static int size = 32;
 
-    public ChunkVoxelData(int size)
+    Dictionary<VertexSignature, int> verticesDict = new Dictionary<VertexSignature, int>();
+    int vertexCount = 0;
+
+    List<int> triangles = new List<int>();
+
+    Vector3[] _normals;
+    Vector3[] _uvs;
+    Vector3[] _vertices;
+
+    Mesh mesh;
+    public MeshFilter meshFilter;
+    public MeshCollider meshCollider;
+
+    public bool threadFinished = true;
+    List<Action> functionsQueue = new List<Action>();
+
+    public int blocksGenerated = 0;
+
+
+    public bool ready = false;
+
+    void Start()
     {
-        this.size = size;
-        raw = new int[size * size * size];
+        mesh = new Mesh();
+        meshFilter = GetComponent<MeshFilter>();
+        meshCollider = GetComponent<MeshCollider>();
+        meshFilter.mesh = mesh;
+        mesh.MarkDynamic();
     }
 
+    private void Update()
+    {
+        while (functionsQueue.Count > 0)
+        {
+            Action func = functionsQueue[0];
+            functionsQueue.RemoveAt(0);
+            func();
+        }
+    }
+
+    public void SetRaw(int[] arr)
+    {
+        this.raw = arr;
+    }
+
+    public void RegenerateAsync()
+    {
+        Async(ChunkUpdate);
+    }
+    public void RegenerateSync()
+    {
+        ChunkUpdate();
+    }
+
+    public void ChunkUpdate()
+    {
+        MakeChunk();
+        PrepareMeshData();
+
+        int[] triArr = triangles.ToArray();
+
+        Action toThread = () =>
+        {
+            mesh.Clear();
+            mesh.vertices = _vertices;
+            mesh.normals = _normals;
+            mesh.triangles = triArr;
+            mesh.SetUVs(0, new List<Vector3>(_uvs));
+
+            meshCollider.sharedMesh = mesh;
+            threadFinished = true;
+        };
+
+        functionsQueue.Add(toThread);
+    }
+    public void MakeChunk()
+    {
+        blocksGenerated = 0;
+        vertexCount = 0;
+        verticesDict.Clear();
+        triangles.Clear();
+
+        int len = size * size * size;
+
+        for (int i = 0; i < len; i++)
+        {
+            int cubeType = raw[i];
+            if (cubeType != 0)
+            {
+                int x = i % size;
+                int y = (i / size) % size;
+                int z = i / (size * size);
+                MakeCube(x, y, z, cubeType);
+                blocksGenerated++;
+            }
+        }
+    }
+
+    void MakeCube(int x, int y, int z, int cubeType)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Direction dir = (Direction)i;
+            if (GetNeighbor(x, y, z, dir) == 0)
+            {
+                MakeFace(dir, x, y, z, cubeType);
+            }
+        }
+    }
+    void MakeFace(Direction dir, int x, int y, int z, int cubeType)
+    {
+        VertexSignature signature;
+        signature.normal = dir;
+
+        Vector3[] faceVertices = CubeMeshData.faceVertices((int)dir, x, y, z);
+        int[] triangleIndices = new int[4];
+        signature.cubeType = cubeType;
+
+        for (int i = 0; i < 4; i++)
+        {
+            signature.position = faceVertices[i];
+
+            int vertex = GetVertexIndex(signature);
+            //int vertex2 = GetVertexEntry(signature);
+
+            triangleIndices[i] = vertex;
+        }
+
+        triangles.AddRange(new int[6] {
+            triangleIndices[0],
+            triangleIndices[1],
+            triangleIndices[2],
+            triangleIndices[0],
+            triangleIndices[2],
+            triangleIndices[3]
+            }
+        );
+
+    }
+    void PrepareMeshData()
+    {
+        _vertices = new Vector3[vertexCount];
+        _normals = new Vector3[vertexCount];
+        _uvs = new Vector3[vertexCount];
+
+        foreach (var pair in verticesDict)
+        {
+            int index = pair.Value;
+            CubeMeshData.DataCoordinate coord = CubeMeshData.offsets[(int)pair.Key.normal];
+            Vector3 uv = CubeMeshData.ProjectPositionToUV(pair.Key.position, pair.Key.normal);
+            uv.z = pair.Key.cubeType;
+
+            _vertices[index] = pair.Key.position;
+            _normals[index] = new Vector3(coord.x, coord.y, coord.z);
+            _uvs[index] = uv;
+        }
+    }
+    public void Async(Action func)
+    {
+        Thread thread = new Thread(new ThreadStart(func));
+        thread.Start();
+        threadFinished = false;
+    }
+    struct VertexSignature
+    {
+        public Vector3 position;
+        public Direction normal;
+        public int cubeType;
+
+        public override int GetHashCode()
+        {
+            int x = (int)(position.x * 2);
+            int y = (int)(position.y * 2);
+            int z = (int)(position.z * 2);
+
+            int n = (int)normal;
+            int c = cubeType;
+
+            int p = (((x + y * size * 2) * size * 2 + z) * 6 + n) * 4 + c;
+            return p;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            return obj.GetHashCode() == GetHashCode();
+        }
+    };
+    int GetVertexIndex(VertexSignature signature)
+    {
+        int index;
+        if (!verticesDict.TryGetValue(signature, out index))
+        {
+            index = vertexCount++;
+            verticesDict.Add(signature, index);
+        }
+
+        return index;
+    }
 
     public int GetCell(int x, int y, int z)
     {
         return raw[x + size * (y + size * z)];
     }
-
     public void SetCell(int x, int y, int z, int val)
     {
         raw[x + size * (y + size * z)] = val;
@@ -48,8 +244,6 @@ public class ChunkVoxelData : MonoBehaviour
 
         return GetCell(neighborCoordinate.x, neighborCoordinate.y, neighborCoordinate.z);
     }
-
-
 }
 
 public enum Direction
--- Assets/Game Scripts/TerrainGeneratorAsync.cs
+++ Assets/Game Scripts/TerrainGeneratorAsync.cs
@@ -8,274 +8,111 @@ using Unity.Mathematics;
 
 public class TerrainGeneratorAsync : MonoBehaviour
 {
-    private float lastScale = 0, lastHeight = 0;
+
     [Range(0, 0.1f)]
     public float scale = 0.01f;
     [Range(1, 32)]
 
     public int maxTerrainHeight = 10;
-    private int lastWorld;
-    public static int chunkSize = 32;
     public int blocksGenerated = 0;
-    
-    Dictionary<VertexSignature, int> verticesDict = new Dictionary<VertexSignature, int>();
-    int vertexCount = 0;
 
-    List<int> triangles = new List<int>();
-   
-    Vector3[] _normals;
-    Vector3[] _uvs;
-    Vector3[] _vertices;
-
-    Mesh mesh;
-    private MeshFilter meshFilter;
-    private MeshCollider meshCollider;
-
-    public List<ChunkVoxelData> chunks;
-    ChunkVoxelData data;
+    public Dictionary<Vector2, ChunkVoxelData> chunks = new Dictionary<Vector2, ChunkVoxelData>();
+    //public ChunkVoxelData data;
 
     public bool threadFinished = true;
 
+    public GameObject chunkPrefab;
+
     void Start()
     {
-
-        lastScale = scale;
-        lastHeight = maxTerrainHeight;
-
-        mesh = new Mesh();
-        meshFilter = GetComponent<MeshFilter>();
-        meshCollider = GetComponent<MeshCollider>();
-        meshFilter.mesh = mesh;
-        mesh.MarkDynamic();
-
-        InitChunkData();
-        ChunkUpdate();
-
-        //AssetDatabase.CreateAsset(mesh, "Assets/Temp/mesh.asset");
+        for(int x = 0; x < 100; x++)
+        {
+            for (int z = 0; z < 100; z++)
+            {
+                MakeChunkAt(x, z);
+            }
+        }
+        
     }
 
     List<Action> functionsQueue = new List<Action>();
 
-
     private void Update()
     {
-        if (lastScale != scale || lastHeight != maxTerrainHeight)
-        {
-            lastScale = scale;
-            lastHeight = maxTerrainHeight;
-            if (threadFinished)
-            {
-                //SetWorldData();
-                //Async(ChunkUpdate);
-            }
-        }
-        while(functionsQueue.Count > 0){
-            Action func = functionsQueue[0];
-            functionsQueue.RemoveAt(0);
-            func();
-
-        }
+        //while(functionsQueue.Count > 0){
+        //    Action func = functionsQueue[0];
+        //    functionsQueue.RemoveAt(0);
+        //    func();
+        //}
     }
+
+    public void MakeChunkAt(int x, int z)
+    {
+        ChunkVoxelData data = Instantiate(chunkPrefab, new Vector3(x * 32 + 0.5f, 0.5f, z * 32 + 0.5f), Quaternion.Euler(0, 0, 0), transform).GetComponent<ChunkVoxelData>();
+        chunks.Add(new Vector2(x, z), data);
+
+        data.SetRaw(InitChunkData(x, z));
+        data.RegenerateSync();
+    }
+
     public void EditWorld(int x, int y, int z, int cubeType)
     {
-        if (threadFinished)
-        {
-            data.SetCell(x, y, z, cubeType);
+        int chunkX = x % 32;
+        int chunkZ = z % 32;
 
-            Async(ChunkUpdate);
-            //ChunkUpdate();
+        int chunkPosX = x / 32;
+        int chunkPosZ = z / 32;
+
+        //Debug.Log("raw coord: " + new Vector2(x, z));
+
+        //Debug.Log("chunk coord: " + new Vector2(chunkX, chunkZ));
+
+        ChunkVoxelData data = null;
+        chunks.TryGetValue(new Vector2(chunkPosX, chunkPosZ), out data);
+        if (data)
+        {
+            if (data.threadFinished)
+            {
+              
+                Debug.Log("put");
+            }
+            data.SetCell(chunkX, y, chunkZ, cubeType);
+            data.RegenerateSync();
+        }
+        else
+        {
+            Debug.LogError("Chunk not found");
         }
     }
 
-    void InitChunkData()
+    public int[] InitChunkData(int chunkPosX, int chunkPosZ)
     {
-        data = new ChunkVoxelData(chunkSize);
-
-        for (int x = 0; x < chunkSize; x++)
+        int size = 32;
+        int[] raw = new int[size * size * size];
+        for (int x = 0; x < size; x++)
         {
-            for (int z = 0; z < chunkSize; z++)
+            for (int z = 0; z < size; z++)
             {
-                int yVal = GetNoiseValue(z, x);
+                int yVal = GetNoiseValue(chunkPosZ * 32 + z, chunkPosX * 32 + x);
 
                 for (int y = yVal - 1; y >= 0; y--)
                 {
                     int cubeType = 4 - Mathf.FloorToInt((float)yVal / (float)maxTerrainHeight * 4);
-                    data.SetCell(x, y, z, cubeType);
+                    raw[x + size * (y + size * z)] = cubeType;
                 }
             }
         }
+        return raw;
 
-    }
-
-    public void ChunkUpdate() {
-        MakeChunk();
-        PrepareMeshData();
-
-        int[] triArr = triangles.ToArray();
-
-        Action toThread = () =>
-        {
-            mesh.Clear();
-            mesh.vertices = _vertices;
-            mesh.normals = _normals;
-            mesh.triangles = triArr;
-            mesh.SetUVs(0, new List<Vector3>(_uvs));
-
-            meshCollider.sharedMesh = mesh;
-            threadFinished = true;
-        };
-
-        functionsQueue.Add(toThread);
-    }
-
-    public void MakeChunk()
-    {
-        blocksGenerated = 0;
-        vertexCount = 0;
-        verticesDict.Clear();
-        triangles.Clear();
-
-        int len = chunkSize * chunkSize * chunkSize;
-
-        for (int i = 0; i < len; i++)
-        {
-            int cubeType = data.raw[i];
-            if (cubeType != 0)
-            {
-                int x = i % chunkSize;
-                int y = (i / chunkSize) % chunkSize;
-                int z = i / (chunkSize * chunkSize);
-                MakeCube(x, y, z, cubeType);
-                blocksGenerated++;
-            }
-        }
-    }
-    void MakeCube(int x, int y, int z, int cubeType)
-    {
-        for (int i = 0; i < 6; i++)
-        {
-            Direction dir = (Direction)i;
-            if (data.GetNeighbor(x, y, z, dir) == 0)
-            {
-                MakeFace(dir, x, y, z, cubeType);
-            }
-        }
-    }
-    void MakeFace(Direction dir, int x, int y, int z, int cubeType)
-    {
-        VertexSignature signature;
-        signature.normal = dir;
-
-        Vector3[] faceVertices = CubeMeshData.faceVertices((int)dir, x, y, z);
-        int[] triangleIndices = new int[4];
-        signature.cubeType = cubeType;
-
-        for (int i = 0; i < 4; i++)
-        {
-            signature.position = faceVertices[i];
-
-            int vertex = GetVertexIndex(signature);
-            //int vertex2 = GetVertexEntry(signature);
-
-            triangleIndices[i] = vertex;
-        }
-
-        triangles.AddRange(new int[6] {
-            triangleIndices[0],
-            triangleIndices[1],
-            triangleIndices[2],
-            triangleIndices[0],
-            triangleIndices[2],
-            triangleIndices[3]
-            }
-        );
-
-
-    }
-    void PrepareMeshData()
-    {
-        //_vertices = vertices.ToArray();
-
-        //Debug.Log("Vertex array entries: " + vertexEntriesList.Count);
-        //Debug.Log(vertexCount2);
-        //Debug.Log("Vertex dictionary entries: " + vertexCount);
-
-
-        _vertices = new Vector3[vertexCount];
-        _normals = new Vector3[vertexCount];
-        _uvs = new Vector3[vertexCount];
-
-        foreach (var pair in verticesDict)
-        {
-            int index = pair.Value;
-            CubeMeshData.DataCoordinate coord = CubeMeshData.offsets[(int)pair.Key.normal];
-            Vector3 uv = CubeMeshData.ProjectPositionToUV(pair.Key.position, pair.Key.normal);
-            uv.z = pair.Key.cubeType;
-
-            _vertices[index] = pair.Key.position;
-            _normals[index] = new Vector3(coord.x, coord.y, coord.z);
-            _uvs[index] = uv;
-        }
-
-        //for (int i = 0; i < vertexCount2; i++)
-        //{
-        //    VertexSignature signature = vertexEntriesList[i];
-        //    CubeMeshData.DataCoordinate coord = CubeMeshData.offsets[(int)signature.normal];
-        //    Vector3 uv = CubeMeshData.ProjectPositionToUV(signature.position, signature.normal);
-        //    uv.z = signature.cubeType;
-
-        //    _vertices[i] = signature.position;
-        //    _normals[i] = new Vector3(coord.x, coord.y, coord.z);
-        //    _uvs[i] = uv;
-        //}
     }
     public void Async(Action func) {
         Thread thread = new Thread(new ThreadStart(func));
         thread.Start();
         threadFinished = false;
     }
-    struct VertexSignature
-    {
-        public Vector3 position;
-        public Direction normal;
-        public int cubeType;
-
-        public override int GetHashCode()
-        {
-            int x = (int)(position.x * 2);
-            int y = (int)(position.y * 2);
-            int z = (int)(position.z * 2);
-
-            int n = (int)normal;
-            int c = cubeType;
-
-            int p = (((x + y * chunkSize * 2) * chunkSize * 2 + z) * 6 + n) * 4 + c;
-            return p;
-        }
-        public override bool Equals(object obj)
-        {
-            if(obj == null)
-            {
-                return false;
-            }
-            return obj.GetHashCode() == GetHashCode();
-        }
-    };
     int GetNoiseValue(float x, float y)
     {
         return Mathf.FloorToInt(Mathf.PerlinNoise(x * scale, y * scale) * maxTerrainHeight);
     }
-    int GetVertexIndex(VertexSignature signature)
-    {
-        int index;
-        if (!verticesDict.TryGetValue(signature, out index))
-        {
-            index = vertexCount++;
-            verticesDict.Add(signature, index);
-        }
-
-        return index;
-    }
-
 
 }
