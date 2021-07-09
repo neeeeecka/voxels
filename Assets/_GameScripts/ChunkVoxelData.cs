233508ea901db2812465a3b155bfc019dfd8a50f
Nika Gabisonia <blackshtormx@gmail.com>
2021-07-09 16:01:40 +0400

    Encapsulated chunk data into struct

---
+++ Assets/_GameScripts/ChunkVoxelData.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+using Unity.Jobs;
+using Unity.Collections;
+struct ChunkUpdateJob : IJobParallelFor
+{
+    public NativeArray<ChunkData> chunkDataArray;
+
+    public void Execute(int index)
+    {
+        var data = chunkDataArray[index];
+        data.Update();
+        chunkDataArray[index] = data;
+    }
+}
+
+public struct ChunkData
+{
+    public bool isEmpty;
+    private int[] raw;
+    Dictionary<VertexSignature, int> verticesDict;
+    public int blocksGenerated;
+    int vertexCount;
+    public List<int> triangles;
+    public Vector3[] _normals;
+    public Vector4[] _uvs;
+    public Vector3[] _vertices;
+    int verticesCounter;
+
+    public ChunkData(ChunkVoxelData chunkVoxelData)
+    {
+        isEmpty = true;
+        blocksGenerated = 0;
+        vertexCount = 0;
+        verticesCounter = 0;
+        triangles = new List<int>();
+        _normals = new Vector3[0];
+        _uvs = new Vector4[0];
+        _vertices = new Vector3[0];
+        this.verticesDict = new Dictionary<VertexSignature, int>();
+
+        this.raw = chunkVoxelData.raw;
+    }
+
+    public void Update()
+    {
+        isEmpty = true;
+        verticesCounter = 0;
+        _normals = new Vector3[0];
+        _uvs = new Vector4[0];
+        _vertices = new Vector3[0];
+
+        MakeChunk();
+        PrepareMeshData();
+    }
+
+    void MakeChunk()
+    {
+        // public bool MakeChunk(bool needsGlobalChange)
+        // {
+        blocksGenerated = 0;
+        vertexCount = 0;
+        verticesDict.Clear();
+        triangles.Clear();
+
+        int len = 16 * 16 * 16;
+
+        for (int i = 0; i < len; i++)
+        {
+            int cubeType = raw[i];
+            if (cubeType != 0)
+            {
+                int x = i % 16;
+                int y = (i / 16) % 16;
+                int z = i / (16 * 16);
+                MakeCube(x, y, z, cubeType);
+                blocksGenerated++;
+            }
+        }
+    }
+
+    void MakeCube(int x, int y, int z, int cubeType)
+    {
+        // NeighborGetter neighborGetter = needsGlobalChange ? globalNeighborGetter : localNeighborGetter;
+
+
+        bool hasBlockUpwards = GetLocalNeighbor(x, y, z, Direction.Up) != 0;
+
+        for (int i = 0; i < 6; i++)
+        {
+            Direction dir = (Direction)i;
+            int textureType = 1;
+            //textureIndex = realIndex + 1 cause air = 0
+            // textureType = 8;
+            //TODO: Change for custom behaviour for each block
+            if (cubeType == 1)
+            {
+                if (dir == Direction.Up)
+                {
+                    textureType = 5; //grass top
+                }
+                else if (!hasBlockUpwards)
+                {
+                    textureType = 9; //grass side
+                }
+
+                // textureType = 5;
+
+            }
+            if (GetLocalNeighbor(x, y, z, dir) == 0)
+            {
+                MakeFace(dir, x, y, z, textureType);
+            }
+        }
+
+    }
+    void MakeFace(Direction dir, int x, int y, int z, int cubeType)
+    {
+        VertexSignature signature;
+        signature.normal = dir;
+
+        Vector3[] faceVertices = CubeMeshData.faceVertices((int)dir, x, y, z);
+        int[] triangleIndices = new int[4];
+        signature.cubeType = (char)cubeType;
+
+        for (int i = 0; i < 4; i++)
+        {
+            signature.index = verticesCounter;
+            verticesCounter++;
+
+            signature.position = faceVertices[i];
+
+            int vertex = GetVertexIndex(signature);
+            //int vertex2 = GetVertexEntry(signature);
+
+            triangleIndices[i] = vertex;
+        }
+
+        triangles.AddRange(new int[6] {
+            triangleIndices[0],
+            triangleIndices[1],
+            triangleIndices[2],
+            triangleIndices[0],
+            triangleIndices[2],
+            triangleIndices[3]
+            }
+        );
+
+    }
+
+    public void PrepareMeshData()
+    {
+        _vertices = new Vector3[vertexCount];
+        _normals = new Vector3[vertexCount];
+        _uvs = new Vector4[vertexCount];
+
+        foreach (var pair in verticesDict)
+        {
+            int index = pair.Value;
+            CubeMeshData.DataCoordinate coord = CubeMeshData.offsets[(int)pair.Key.normal];
+            Vector4 uv = CubeMeshData.ProjectPositionToUV(pair.Key.position, pair.Key.normal);
+
+            uv.w = pair.Key.cubeType;
+            // uv.w = 9;
+
+            _vertices[index] = pair.Key.position;
+            _normals[index] = new Vector3(coord.x, coord.y, coord.z);
+
+            //get vertex chunk position
+            Vector3 chunkPos = pair.Key.position;
+            Vector3 normal = CubeMeshData.offsets[(int)pair.Key.normal].ToVector();
+
+            _uvs[index] = uv;
+
+        }
+
+    }
+
+    int GetVertexIndex(VertexSignature signature)
+    {
+        int index;
+        if (!verticesDict.TryGetValue(signature, out index))
+        {
+            index = vertexCount++;
+            verticesDict.Add(signature, index);
+        }
+
+        return index;
+    }
+
+    public int GetCell(int x, int y, int z)
+    {
+        return raw[x + 16 * (y + 16 * z)];
+    }
+    public void SetCell(int x, int y, int z, int val)
+    {
+        if (val != 0)
+        {
+            isEmpty = false;
+        }
+        raw[x + 16 * (y + 16 * z)] = val;
+    }
+    public int GetLocalNeighbor(int x, int y, int z, Direction dir)
+    {
+        CubeMeshData.DataCoordinate checkOffset = CubeMeshData.offsets[(int)dir];
+        CubeMeshData.DataCoordinate neighborCoordinate = new CubeMeshData.DataCoordinate(
+            x + checkOffset.x,
+            y + checkOffset.y,
+            z + checkOffset.z
+        );
+
+        if (neighborCoordinate.x < 0 || neighborCoordinate.x >= 16)
+        {
+            return 0;
+        }
+        if (neighborCoordinate.y < 0 || neighborCoordinate.y >= 16)
+        {
+            return 0;
+        }
+        if (neighborCoordinate.z < 0 || neighborCoordinate.z >= 16)
+        {
+            return 0;
+        }
+
+        return GetCell(neighborCoordinate.x, neighborCoordinate.y, neighborCoordinate.z);
+    }
+}
+
+public class ChunkVoxelData : MonoBehaviour
+{
+
+    public Vector3 chunkPos = Vector3.zero;
+    public int[] raw = new int[16 * 16 * 16];
+    public static int size = 16;
+
+    Mesh mesh;
+    public MeshFilter meshFilter;
+    public MeshCollider meshCollider;
+
+    public MeshRenderer meshRenderer;
+
+    public bool threadFinished = true;
+    List<Action> functionsQueue = new List<Action>();
+
+    public int blocksGenerated = 0;
+    public TerrainGeneratorAsync terrain;
+
+    List<int> triangles = new List<int>();
+
+    Vector3[] _normals;
+    Vector4[] _uvs;
+    Vector3[] _vertices;
+    public bool isEmpty;
+
+    void Start()
+    {
+
+        mesh = new Mesh();
+        meshFilter = GetComponent<MeshFilter>();
+        meshCollider = GetComponent<MeshCollider>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshFilter.mesh = mesh;
+        mesh.MarkDynamic();
+        RegenerateAsync(true);
+
+        var data = new ChunkData(this);
+        data.Update();
+        ChunkUpdate(data);
+
+    }
+
+    private void Update()
+    {
+        while (functionsQueue.Count > 0)
+        {
+            Action func = functionsQueue[0];
+            functionsQueue.RemoveAt(0);
+            func();
+        }
+    }
+
+    public void SetRaw(int[] arr, bool isEmpty)
+    {
+        this.isEmpty = isEmpty;
+        this.raw = arr;
+    }
+
+    public void RegenerateAsync(bool needsGlobalChange)
+    {
+        //Debug.Log("Needs global change: " + needsGlobalChange);
+        //Debug.Log("chunk " + chunkPos);
+
+        needsGlobalChange = true;
+        // Async(ChunkUpdate, needsGlobalChange);
+        // ChunkUpdate(needsGlobalChange);
+    }
+
+    int verticesCounter = 0;
+
+    public int GetCell(int x, int y, int z)
+    {
+        return raw[x + 16 * (y + 16 * z)];
+    }
+    public void SetCell(int x, int y, int z, int val)
+    {
+        if (val != 0)
+        {
+            isEmpty = false;
+        }
+        raw[x + 16 * (y + 16 * z)] = val;
+    }
+
+    void ChunkUpdate(ChunkData chunkData)
+    {
+        // public void ChunkUpdate(bool needsGlobalChange)
+        // {
+        // MakeChunk(needsGlobalChange);
+        // PrepareMeshData();
+
+        // ChunkData chunkData = new ChunkData(this);
+        chunkData.Update();
+        // Debug.Log();
+        blocksGenerated = chunkData.blocksGenerated;
+        _vertices = chunkData._vertices;
+        _normals = chunkData._normals;
+        triangles = chunkData.triangles;
+        _uvs = chunkData._uvs;
+
+        int[] triArr = triangles.ToArray();
+
+        // Action toMainThread = () =>
+        // {
+        mesh.Clear();
+        mesh.vertices = _vertices;
+        mesh.normals = _normals;
+        mesh.triangles = triArr;
+        mesh.SetUVs(0, new List<Vector4>(_uvs));
+        mesh.RecalculateTangents();
+
+        meshRenderer.shadowCastingMode = ShadowCastingMode.On;
+        meshCollider.sharedMesh = mesh;
+        // threadFinished = true;
+
+        // AssetDatabase.CreateAsset(mesh, "Assets/_Generated/" + "chunkie.asset");
+        // AssetDatabase.SaveAssets();
+        // };
+
+        // functionsQueue.Add(toMainThread);
+        // }
+    }
+
+
+
+    // public int GetGlobalNeighbor(int x, int y, int z, Direction dir)
+    // {
+    //     x = x + size * (int)chunkPos.x;
+    //     y = y + size * (int)chunkPos.y;
+    //     z = z + size * (int)chunkPos.z;
+
+    //     CubeMeshData.DataCoordinate checkOffset = CubeMeshData.offsets[(int)dir];
+    //     CubeMeshData.DataCoordinate neighborCoordinate = new CubeMeshData.DataCoordinate(
+    //         x + checkOffset.x,
+    //         y + checkOffset.y,
+    //         z + checkOffset.z
+    //     );
+
+    //     return terrain.GetBlockAt(neighborCoordinate.x, neighborCoordinate.y, neighborCoordinate.z);
+    // }
+
+}
+
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West,
+    Up,
+    Down
+}
+struct VertexSignature
+{
+    public Vector3 position;
+    public Direction normal;
+    public char cubeType;
+    public int index;
+
+    //Blocks will share vertices if faces have same normals and position 
+    // public override int GetHashCode()
+    // {
+    //     char x = (char)(position.x * 2);
+    //     char y = (char)(position.y * 2);
+    //     char z = (char)(position.z * 2);
+
+    //     char n = (char)normal;
+    //     char c = cubeType;
+
+    //     // int p = (((x + y * size * 2) * size * 2 + z) * 6 + n) * 4 + c;
+    //     int p = (((x + y * size * 2) * size * 2 + z) * 6 + n);
+    //     //ignore cube type, 
+    //     //treat as same block
+    //     //...cant UV map that thing
+
+    //     return p;
+    // }
+
+    //will use quads before I figure out how to do UV mapping for optimized meshes
+    public override int GetHashCode()
+    {
+        return index;
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        return obj.GetHashCode() == GetHashCode();
+    }
+};
+
---
+++ Assets/_GameScripts/TerrainGeneratorAsync.cs
@@ -0,0 +1,499 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Threading;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Collections;
+public class TerrainGeneratorAsync : MonoBehaviour
+{
+    public Vector3 worldDimensions = new Vector3(3, 3, 3);
+
+    private float oldExp = 0;
+    public float exponent = 2;
+
+    public int terraces = 0;
+
+    public int waterlevel = 5;
+
+    private float oldms = 0;
+    [Range(0, 1f)]
+    public float mountainScale = 0.01f;
+
+    private float oldss = 0;
+    [Range(0, 1f)]
+    public float stonesScale = 0.1f;
+
+    private float oldds = 0;
+    [Range(0, 1f)]
+    public float detailScale = 0.2f;
+
+    public int maxTerrainHeight = 10;
+    public int blocksGenerated = 0;
+
+    public Dictionary<Vector3, ChunkVoxelData> chunks = new Dictionary<Vector3, ChunkVoxelData>();
+    //public int[] chunks = new int[3 * 3 * 3];
+
+    public bool threadFinished = true;
+
+    public GameObject chunkPrefab;
+
+    void Start()
+    {
+        // int size = 16;
+
+        // int x = -1;
+        // int y = 2;
+        // int z = -1;
+
+        // int chunkRelativeX = x % size;
+        // int chunkRelativeY = y % size;
+        // int chunkRelativeZ = z % size;
+
+        // if (x < 0)
+        // {
+        //     chunkRelativeX += size;
+        // }
+        // if (z < 0)
+        // {
+        //     chunkRelativeZ += size;
+        // }
+
+        // int chunkPosX = Mathf.FloorToInt((float)x / size);
+        // int chunkPosY = Mathf.FloorToInt((float)y / size);
+        // int chunkPosZ = Mathf.FloorToInt((float)z / size);
+
+        // Debug.Log(new Vector3(chunkRelativeX, chunkRelativeY, chunkRelativeZ));
+        // Debug.Log(new Vector3(chunkPosX, chunkPosY, chunkPosZ));
+
+        // for (int x = 0; x < worldDimensions.x; x++)
+        // {
+        //     for (int y = 0; y < worldDimensions.y; y++)
+        //     {
+        //         for (int z = 0; z < worldDimensions.z; z++)
+        //         {
+        //             // chunkUpdateQueue.Add(MakeChunkAt(x, y, z));
+        //         }
+        //     }
+        // }
+
+        // chunkUpdateQueue.Add(MakeChunkAt(12, 2, 12));
+
+
+        // UpdateNextChunk();
+
+
+        // if (c < 20)
+        // {
+        //     ChunkVoxelData ch = GetChunk(-1, 0, 0);
+        //     Debug.Log(ch);
+        // }
+
+
+    }
+
+    List<Action> functionsQueue = new List<Action>();
+    List<ChunkVoxelData> chunkUpdateQueue = new List<ChunkVoxelData>();
+
+    public Transform player;
+
+
+    int c = 0;
+
+
+    private void Update()
+    {
+
+
+        // for (int x = 0; x < 2; x++)
+        // {
+        //     ChunkVoxelData ch;
+        //     chunks.TryGetValue(new Vector3(x, 0, 5), out ch);
+
+        //     if (ch == null)
+        //     {
+        //         Debug.Log("created");
+        //         chunkUpdateQueue.Add(MakeChunkAt(x, 0, 5));
+        //     }
+        // }
+
+
+        int radius = 6;
+        int unit = ChunkVoxelData.size;
+
+        Vector3 viewPos = player.transform.position / unit;
+
+        foreach (KeyValuePair<Vector3, ChunkVoxelData> chunk in chunks)
+        {
+            // if (Vector3.Distance(player.transform.TransformPoint(chunk.Value.transform.position), player.transform.position) > 6 * unit)
+            // {
+            //     chunk.Value.transform.gameObject.SetActive(false);
+            //     // threadFinished = false;
+            //     // chunks.Remove(chunk.Key);
+            //     // threadFinished = true;
+            // }
+            // else
+            // {
+            //     chunk.Value.transform.gameObject.SetActive(true);
+            // }
+        }
+
+        for (int x = -radius; x < radius; x++)
+        {
+            for (int z = -radius; z < radius; z++)
+            {
+                int realX = (int)viewPos.x + x;
+                int realZ = (int)viewPos.z + z;
+
+                Vector3 chunkPos = new Vector3(realX, 0, realZ);
+                ChunkVoxelData chunk;
+                chunks.TryGetValue(chunkPos, out chunk);
+
+                if (chunk == null)
+                {
+                    for (int y = 0; y < 3; y++)
+                    {
+                        // chunkUpdateQueue.Add(MakeChunkAt(realX, y, realZ));
+                        MakeChunkAt(realX, y, realZ);
+
+                        // var chunkDataArray = new NativeArray<ChunkData>(chunks.Count, Allocator.TempJob);
+                        // int i = 0;
+                        // foreach (KeyValuePair<Vector3, ChunkVoxelData> c in chunks)
+                        // {
+                        //     chunkDataArray[i] = new ChunkData(c.Value);
+                        //     i++;
+                        // }
+                        // var job = new ChunkUpdateJob
+                        // {
+                        //     chunkDataArray = chunkDataArray
+                        // };
+                        // var handle = job.Schedule(chunks.Count, 1);
+                        // handle.Complete();
+                        // chunkDataArray.Dispose();
+                    }
+                }
+
+            }
+        }
+
+
+
+
+        // var chunkDataArray = new NativeArray<ChunkVoxelData.Data>(chunks.Count, Allocator.TempJob);
+
+        // int i = 0;
+        // foreach (KeyValuePair<Vector3, ChunkVoxelData> chunk in chunks)
+        // {
+        //     chunkDataArray[i] = new ChunkVoxelData.Data(chunk.Value);
+        //     i++;
+        // }
+        // var chunkUpdateJob = new ChunkUpdateJob
+        // {
+        //     chunkDataArray = chunkDataArray
+        // };
+
+        // var jobHandle = chunkUpdateJob.Schedule(chunks.Count, 1);
+        // jobHandle.Complete();
+        // chunkDataArray.Dispose();
+
+        // if (threadFinished)
+        // {
+        //     UpdateNextChunk();
+        // }
+
+        // while (functionsQueue.Count > 0)
+        // {
+        //     Action func = functionsQueue[0];
+        //     functionsQueue.RemoveAt(0);
+        //     func();
+        // }
+    }
+
+    private Square getChunkSquare(Vector3 pos)
+    {
+        Square square = new Square();
+        square.x = (int)pos.x;
+        square.y = (int)pos.z;
+        square.w = 1;
+
+        return square;
+    }
+
+    private bool intersects(Square a, Square b)
+    {
+        if (a.x >= b.x && a.x <= b.x + 1 && a.y >= b.y && a.y <= b.y + 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    public ChunkVoxelData MakeChunkAt(int x, int y, int z)
+    {
+        GameObject chunk = Instantiate(chunkPrefab,
+            new Vector3(
+                x * ChunkVoxelData.size + 0.5f,
+                y * ChunkVoxelData.size + 0.5f,
+                z * ChunkVoxelData.size + 0.5f
+                ), Quaternion.Euler(0, 0, 0), transform);
+        chunk.name = "chunk: (" + x + "." + y + "." + z + ")";
+        ChunkVoxelData data = chunk.GetComponent<ChunkVoxelData>();
+        data.terrain = this;
+        data.chunkPos = new Vector3(x, y, z);
+        chunks.Add(new Vector3(x, y, z), data);
+
+        //data.SetRaw(InitChunkData(x, y, z));
+        InitChunkData(x, y, z, data);
+        return data;
+    }
+
+    // private void RegenerateSyncWrapper(ChunkVoxelData data)
+    // {
+    //     if (data.isEmpty)
+    //     {
+    //         data.threadFinished = true;
+    //     }
+    //     else
+    //     {
+    //         data.RegenerateSync(true);
+    //     }
+
+
+    //     Action toMainThread = () =>
+    //     {
+    //         threadFinished = true;
+    //         chunkUpdateQueue.RemoveAt(0);
+    //         UpdateNextChunk();
+    //     };
+    //     functionsQueue.Add(toMainThread);
+    // }
+
+    // private void UpdateNextChunk()
+    // {
+    //     if (chunkUpdateQueue.Count >= 1)
+    //     {
+    //         Async(RegenerateSyncWrapper, chunkUpdateQueue[0]);
+    //         //RegenerateSyncWrapper(chunksQueue[0]);
+    //     }
+    // }
+
+    public ChunkVoxelData GetChunkByWorldPos(int worldX, int worldY, int worldZ)
+    {
+        int size = ChunkVoxelData.size;
+
+        int chunkPosX = Mathf.FloorToInt((float)worldX / size);
+        int chunkPosY = Mathf.FloorToInt((float)worldY / size);
+        int chunkPosZ = Mathf.FloorToInt((float)worldZ / size);
+
+
+        ChunkVoxelData chunk = null;
+        chunks.TryGetValue(new Vector3(chunkPosX, chunkPosY, chunkPosZ), out chunk);
+
+        return chunk;
+    }
+
+    public int GetBlockAt(int x, int y, int z)
+    {
+
+        int size = ChunkVoxelData.size;
+        int chunkX = x % size;
+        int chunkY = y % size;
+        int chunkZ = z % size;
+
+
+        if (x < 0)
+        {
+            chunkX = x & (size - 1);
+        }
+        if (z < 0)
+        {
+            chunkZ = z & (size - 1);
+        }
+
+
+        ChunkVoxelData chunk = GetChunkByWorldPos(x, y, z);
+
+
+        if (chunk == null)
+        {
+            return 0;
+        }
+
+        // Debug.Log(new Vector3(x, y, z));
+        // Debug.Log(new Vector3(chunkX, y, chunkZ));
+        // Debug.Log("pos: " + chunk.chunkPos);
+
+        // Debug.Log(new Vector3(chunkX, y, chunkZ));
+
+        return chunk.GetCell(chunkX, chunkY, chunkZ);
+    }
+
+    public void EditWorld(int x, int y, int z, int cubeType)
+    {
+        int size = ChunkVoxelData.size;
+        //get intra chunk coordinates
+        int chunkX = x % size;
+        int chunkY = y % size;
+        int chunkZ = z % size;
+
+        if (x < 0)
+        {
+            chunkX = x & (size - 1);
+        }
+        if (z < 0)
+        {
+            chunkZ = z & (size - 1);
+        }
+
+        ChunkVoxelData[] adjacentChunks = new ChunkVoxelData[3];
+
+        ChunkVoxelData chunk = GetChunkByWorldPos(x, y, z);
+        bool needsGlobalChange = false;
+
+        if (chunkX == size - 1)
+        {
+            adjacentChunks[0] = GetChunkByWorldPos(x + 1, y, z);
+            needsGlobalChange = true;
+        }
+        if (chunkX == 0)
+        {
+            adjacentChunks[0] = GetChunkByWorldPos(x - 1, y, z);
+            needsGlobalChange = true;
+        }
+
+        if (chunkY == size - 1)
+        {
+            adjacentChunks[1] = GetChunkByWorldPos(x, y + 1, z);
+            needsGlobalChange = true;
+
+        }
+        if (chunkY == 0)
+        {
+            adjacentChunks[1] = GetChunkByWorldPos(x, y - 1, z);
+            needsGlobalChange = true;
+
+        }
+
+        if (chunkZ == size - 1)
+        {
+            adjacentChunks[2] = GetChunkByWorldPos(x, y, z + 1);
+            needsGlobalChange = true;
+
+        }
+        if (chunkZ == 0)
+        {
+            adjacentChunks[2] = GetChunkByWorldPos(x, y, z - 1);
+            needsGlobalChange = true;
+
+        }
+
+
+        if (chunk)
+        {
+            //Debug.Log("chunk found " + x / size + " - " + y / size + " - " + z / size);
+            // if (chunk.threadFinished)
+            // {
+            chunk.SetCell(chunkX, chunkY, chunkZ, cubeType);
+            //chunk.RegenerateSync();
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (adjacentChunks[i] != null)
+                {
+                    // if (adjacentChunks[i].threadFinished && !adjacentChunks[i].isEmpty)
+                    // {
+                    //     adjacentChunks[i].RegenerateAsync(true);
+                    //     //adjacentChunks[i].RegenerateSync(true);
+                    // }
+                    if (!adjacentChunks[i].isEmpty)
+                    {
+                        adjacentChunks[i].RegenerateAsync(true);
+                        //adjacentChunks[i].RegenerateSync(true);
+                    }
+                }
+            }
+            chunk.RegenerateAsync(true);
+            //chunk.RegenerateSync(true);
+            // }
+        }
+        else
+        {
+            Debug.LogError("Chunk not found: " + x + " - " + y + " - " + z);
+        }
+    }
+
+
+
+    int cubeTypes = 5;
+
+    public int[] InitChunkData(int chunkPosX, int chunkPosY, int chunkPosZ, ChunkVoxelData data)
+    {
+        int size = ChunkVoxelData.size;
+        int[] raw = new int[size * size * size];
+        int floor = chunkPosY * size;
+
+        bool isEmpty = true;
+
+        if (chunkPosY * size > maxTerrainHeight)
+        {
+            data.SetRaw(raw, isEmpty);
+            return raw;
+        }
+
+        for (int x = 0; x < size; x++)
+        {
+            for (int z = 0; z < size; z++)
+            {
+                int yVal = GetNoiseValue(
+                    chunkPosZ * size + z,
+                    chunkPosX * size + x
+                    );
+                int cubeType = 1;
+
+                if (yVal / size >= chunkPosY)
+                {
+                    isEmpty = false;
+                }
+
+                for (int y = floor; y < yVal; y++)
+                {
+                    raw[x + size * (y % size + size * z)] = cubeType;
+                }
+            }
+        }
+        data.SetRaw(raw, isEmpty);
+
+        return raw;
+    }
+    public void Async(Action<ChunkVoxelData> func, ChunkVoxelData data)
+    {
+        Thread thread = new Thread(() => func(data));
+        thread.Start();
+        threadFinished = false;
+    }
+    int GetNoiseValue(float x, float y)
+    {
+        float mountains = Mathf.PerlinNoise(x * mountainScale, y * mountainScale);
+        float stones = 0.5f * Mathf.PerlinNoise(x * stonesScale, y * stonesScale) * mountains;
+        float detail = 0.25f * Mathf.PerlinNoise(x * detailScale, y * detailScale) * (stones + mountains);
+
+        float e = Mathf.Clamp(mountains + stones + detail, 0, 1);
+
+        //e = Mathf.Round(e * terraces) / terraces;
+        e = Mathf.Pow(e, exponent);
+
+        return Mathf.FloorToInt(e * maxTerrainHeight);
+    }
+
+}
+
+public struct Square
+{
+    public int x;
+    public int y;
+
+    public int w;
+}
